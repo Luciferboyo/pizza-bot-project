6919f670da7fe1ea55bdc37ce78137ef17b7e5bd
boyo <101465611+Luciferboyo@users.noreply.github.com>
2024-06-06 13:18:48 +0800

    Update Bot.java

--- src/main/java/com/bot/Bot.java
+++ src/main/java/com/bot/Bot.java
@@ -2,8 +2,8 @@ package com.bot;
 
 public class Bot {
 
-    static final String BOT_TOKEN = "7211848059:AAE2DS6y3fDZehtKEqFV3OlfXVPdW64SPVU";
+    static final String BOT_TOKEN = "";
 
-    static final String BOT_USERNAME = "Pizza_Sell_BOT";
+    static final String BOT_USERNAME = "";
 
 }
